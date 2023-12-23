ec370d52758c115387f1bb1fc8e3b93b36dc0962
JaeHyun2020 <polar2000.naver.com>
2023-12-23 15:54:15 +0900

    commit test

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st 
+import pandas as pd 
+import numpy as pd 
+
+from time import sleep 
+
+# 페이지 기본 설정 
+st.set_page_config(
+    page_icon="",
+    page_title="LeeJH's Streamlit Test",
+    layout="wide", 
+)
+
+# 페이지 헤더, 서브 헤더, 제목 설정 
+st.headyyer("Welcome to Website")
+st.subheader("스트림릿 기능 맛보기")
+
---
+++ app2.py
@@ -0,0 +1,4 @@
+import streamlit as st 
+
+# 타이틀
+st.title("타이틀")
